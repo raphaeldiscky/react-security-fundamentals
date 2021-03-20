ad0681d44ad1da6cd9ae4aa1da0f2029ca743439
Raphael Discky <zundria.putra@gmail.com>
2021-03-20 12:56:06 +0700

    Complete the user signup form

--- orbit-app/src/components/common/Input.js
+++ orbit-app/src/components/common/Input.js
@@ -6,7 +6,7 @@ const Input = ({ ariaLabel, name, type, placeholder, field }) => (
     aria-label={ariaLabel}
     name={name}
     type={type}
-    className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5'
+    className='appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5'
     placeholder={placeholder}
   />
 )
--- orbit-app/src/pages/Signup.js
+++ orbit-app/src/pages/Signup.js
@@ -10,6 +10,8 @@ import GradientBar from './../components/common/GradientBar'
 import FormError from './../components/FormError'
 import FormSuccess from './../components/FormSuccess'
 import logo from './../images/logo.png'
+import { publicFetch } from '../util/fetch'
+import { Redirect } from 'react-router'
 
 const SignupSchema = Yup.object().shape({
   firstName: Yup.string().required('First name is required'),
@@ -22,10 +24,18 @@ const Signup = () => {
   const [signupSuccess, setSignupSuccess] = useState()
   const [signupError, setSignupError] = useState()
   const [loginLoading, setLoginLoading] = useState(false)
+  const [redirectOnLogin, setRedirectOnLogin] = useState(false)
 
   const submitCredentials = async (credentials) => {
     try {
       setLoginLoading(true)
+      const { data } = await publicFetch.post('signup', credentials)
+      setSignupSuccess(data.message)
+      setSignupError('')
+      setTimeout(() => {
+        setRedirectOnLogin(true)
+      }, 700)
+      console.log(data)
     } catch (error) {
       setLoginLoading(false)
       const { data } = error.response
@@ -36,6 +46,7 @@ const Signup = () => {
 
   return (
     <>
+      {redirectOnLogin && <Redirect to='/dashboard' />}
       <section className='w-1/2 h-screen m-auto p-8 sm:pt-10'>
         <GradientBar />
         <Card>
