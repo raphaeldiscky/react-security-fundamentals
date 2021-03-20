64cff2da9e6019be9df25ca5792a9b7ef410b18b
Raphael Discky <zundria.putra@gmail.com>
2021-03-20 14:15:27 +0700

    Guard client side routes based on auth state

--- orbit-app/src/App.js
+++ orbit-app/src/App.js
@@ -1,8 +1,13 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import React, { useContext } from 'react'
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch
+} from 'react-router-dom'
 import './App.css'
 import AppShell from './AppShell'
-import { AuthProvider } from './context/AuthContext'
+import { AuthContext, AuthProvider } from './context/AuthContext'
 import { FetchProvider } from './context/FetchContext'
 import Account from './pages/Account'
 import Dashboard from './pages/Dashboard'
@@ -13,6 +18,22 @@ import Login from './pages/Login'
 import Settings from './pages/Settings'
 import Signup from './pages/Signup'
 import Users from './pages/Users'
+
+const AuthenticatedRoute = ({ children, ...rest }) => {
+  const authContext = useContext(AuthContext)
+  return (
+    <Route
+      {...rest}
+      render={() =>
+        authContext.isAuthenticated() ? (
+          <AppShell>{children}</AppShell>
+        ) : (
+          <Redirect to='/' />
+        )
+      }
+    />
+  )
+}
 
 const AppRoutes = () => {
   return (
@@ -26,31 +47,21 @@ const AppRoutes = () => {
       <Route exact path='/'>
         <Home />
       </Route>
-      <Route path='/dashboard'>
-        <AppShell>
-          <Dashboard />
-        </AppShell>
-      </Route>
-      <Route path='/inventory'>
-        <AppShell>
-          <Inventory />
-        </AppShell>
-      </Route>
-      <Route path='/account'>
-        <AppShell>
-          <Account />
-        </AppShell>
-      </Route>
-      <Route path='/settings'>
-        <AppShell>
-          <Settings />
-        </AppShell>
-      </Route>
-      <Route path='/users'>
-        <AppShell>
-          <Users />
-        </AppShell>
-      </Route>
+      <AuthenticatedRoute path='/dashboard'>
+        <Dashboard />
+      </AuthenticatedRoute>
+      <AuthenticatedRoute path='/inventory'>
+        <Inventory />
+      </AuthenticatedRoute>
+      <AuthenticatedRoute path='/account'>
+        <Account />
+      </AuthenticatedRoute>
+      <AuthenticatedRoute path='/settings'>
+        <Settings />
+      </AuthenticatedRoute>
+      <AuthenticatedRoute path='/users'>
+        <Users />
+      </AuthenticatedRoute>
       <Route path='*'>
         <FourOFour />
       </Route>
