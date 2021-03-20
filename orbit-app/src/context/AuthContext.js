a7eb7b80d9453494f9880f6fd0452afa202fd0c8
Raphael Discky <zundria.putra@gmail.com>
2021-03-20 13:17:12 +0700

    Persist auth state on page refresh

--- orbit-app/src/context/AuthContext.js
+++ orbit-app/src/context/AuthContext.js
@@ -4,13 +4,21 @@ const AuthContext = createContext()
 const { Provider } = AuthContext
 
 const AuthProvider = ({ children }) => {
+  // should have put these below to useEffect for best practice
+  const token = localStorage.getItem('token')
+  const userInfo = localStorage.getItem('userInfo')
+  const expiresAt = localStorage.getItem('expiresAt')
+
   const [authState, setAuthState] = useState({
-    token: null,
-    expiresAt: null,
-    userInfo: {}
+    token,
+    expiresAt,
+    userInfo: userInfo ? JSON.parse(userInfo) : {}
   })
 
   const setAuthInfo = ({ token, userInfo, expiresAt }) => {
+    localStorage.setItem('token', token)
+    localStorage.setItem('userInfo', JSON.stringify(userInfo))
+    localStorage.setItem('expiresAt', expiresAt)
     setAuthState({
       token,
       userInfo,
