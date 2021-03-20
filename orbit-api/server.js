6fc8b073496f13bab3a08af8ff1a82e2b5b065fb
Raphael Discky <zundria.putra@gmail.com>
2021-03-20 18:18:21 +0700

    Attach a user to the request object

--- orbit-api/server.js
+++ orbit-api/server.js
@@ -117,13 +117,34 @@ app.post('/api/signup', async (req, res) => {
   }
 })
 
+const attachUser = (req, res, next) => {
+  const token = req.headers.authorization
+  if (!token) {
+    return res.status(401).json({ message: 'Authentication invalid' })
+  }
+  const decodedToken = jwtDecode(token.slice(7))
+  if (!decodedToken) {
+    return res
+      .status(401)
+      .json({ message: 'There was a problem authorizing the request' })
+  } else {
+    req.user = decodedToken
+    next()
+  }
+}
+
+app.use(attachUser)
+
 const checkJwt = jwt({
   secret: process.env.JWT_SECRET,
   issuer: 'api.orbit',
   audience: 'api.orbit'
 })
 
-app.get('/api/dashboard-data', checkJwt, (req, res) => res.json(dashboardData))
+app.get('/api/dashboard-data', checkJwt, (req, res) => {
+  console.log(req.user)
+  return res.json(dashboardData)
+})
 
 app.patch('/api/user-role', async (req, res) => {
   try {
